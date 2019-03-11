31a8cf14c4b9ea900142ac06618c33a8a9183928
UO250898 <32924692+UO250898@users.noreply.github.com>
2019-03-11 21:23:04 +0100

    try outs

---
+++ chat/scripts/main.js
@@ -0,0 +1,66 @@
+const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
+const fileClient = require('solid-file-client');
+
+async function login () {
+  await fileClient.popupLogin().then(webId => {
+      console.log(`Logged in as ${webId}.`);
+  }, err => console.log(err));
+  $('#login').hide();
+  $('#logout').show();
+}
+
+async function logout () {
+  await fileClient.logout().then ( console.log( `Bye now!` ));
+  $('#login').show();
+  $('#logout').hide();
+}
+
+module.exports = {
+  login: login,
+  logout: logout
+}
+
+// Log the user in and out on click
+const popupUri = 'popup.html';
+$('#login  button').click(() => solid.auth.login());
+$('#logout button').click(() => solid.auth.logout());
+
+// Update components to match the user's login status
+solid.auth.trackSession(session => {
+  const loggedIn = !!session;
+  $('#login').toggle(!loggedIn);
+  $('#logout').toggle(loggedIn);
+  if (loggedIn) {
+    $('#user').text(session.webId);
+    // Use the user's WebID as default profile
+    if (!$('#profile').val())
+      $('#profile').val(session.webId);
+  }
+});
+
+$('#view').click(async function loadProfile() {
+  // Set up a local data store and associated data fetcher
+  const store = $rdf.graph();
+  const fetcher = new $rdf.Fetcher(store);
+
+  // Load the person's data into the store
+  const person = $('#profile').val();
+  await fetcher.load(person);
+
+  // Display their details
+  const fullName = store.any($rdf.sym(person), FOAF('name'));
+  $('#fullName').text(fullName && fullName.value);
+
+  // Display their friends
+  const friends = store.each($rdf.sym(person), FOAF('knows'));
+  $('#friends').empty();
+  friends.forEach(async (friend) => {
+    await fetcher.load(friend);
+    const fullName = store.any(friend, FOAF('name'));
+    $('#friends').append(
+      $('<li>').append(
+        $('<a>').text(fullName && fullName.value || friend.value)
+                .click(() => $('#profile').val(friend.value))
+                .click(loadProfile)));
+  });
+});
