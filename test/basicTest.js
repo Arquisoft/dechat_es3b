f4b3f4e035cd3ed4baea9855b0786eb7c6e840ba
Mar Rodríguez Medina <47086968+Maarr@users.noreply.github.com>
2019-03-29 10:36:12 +0100

    Try test

--- test/basicTest.js
+++ test/basicTest.js
@@ -1,8 +1,19 @@
 require("chai");
+const semanticChat=require("../lib/semanticchat");
+const chat= new semanticChat({url:"urlPrueba"});
 var assert = require("assert");
+
+describe("SemanticChat", function () {
+  it("getUrl", function () {
+    assert.equal(chat.geturl(), "urlPrueba");
+  });
+
+});
+
 describe("Test chat", function () {
   it("2 is 2", function () {
     assert.equal(2, 2);
   });
 
 });
+
