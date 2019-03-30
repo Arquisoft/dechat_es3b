17554cd83443ae5a2c75f31302be587aecba649a
AlbaSS18 <albaserenasuarez1998@gmail.com>
2019-03-30 20:39:14 +0100

    create a chat test cucumber

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -23,3 +23,36 @@ When('the user make login', function () {
 Then('the login is successfull {string}', function (expectedReply) {
   assert.equal(this.reply, expectedReply);
 });
+
+
+//------------------ Test create a chat ------------------
+
+function createChat(user,chat,friend) {
+  if(friend === null){
+     return "Friend does not exist";
+  }
+  else{
+      return "Chat created";
+  }
+}
+
+Given('a {string}', function (friend) {
+   var list_friend = ["friendTest3", "friend2"];
+
+    this.friend = null;
+    
+    for (var i = 0; i < list_friend.length; i+=1) {
+        if(list_friend[i] === friend){
+           this.friend = friend;
+        }
+    }
+    
+});
+
+When('a {string} start a {string} with {string}', function (user,chat,friend) {
+  this.message = createChat(this.user,this.chat, this.friend);
+});
+
+Then('the {string} is created {string}', function (chat,expectedReply) {
+  assert.equal(this.message, expectedReply);
+});
