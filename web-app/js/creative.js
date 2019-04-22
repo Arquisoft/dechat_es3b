ef8c5a939f30ccc306c17f2f21a68ec01d34e033
Mar Rodríguez Medina <UO257962@uniovi.es>
2019-04-22 10:40:02 +0200

    Creative fixed

--- web-app/js/creative.js
+++ web-app/js/creative.js
@@ -2,12 +2,12 @@
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  $("a.js-scroll-trigger[href*='#']:not([href='#'])").click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
+        $("html, body").animate({
           scrollTop: (target.offset().top - 72)
         }, 1000, "easeInOutExpo");
         return false;
@@ -16,12 +16,12 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse('hide');
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
+  $("body").scrollspy({
     target: '#mainNav',
     offset: 75
   });
@@ -40,7 +40,7 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
+  $("#portfolio").magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
