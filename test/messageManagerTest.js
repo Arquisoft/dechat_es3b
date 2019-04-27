0af7b35c8000bafc2c19e8cedcd592d4d61caba1
AlbaSS18 <albaserenasuarez1998@gmail.com>
2019-04-27 17:45:28 +0200

    messageManagerTest.js

--- test/messageManagerTest.js
+++ test/messageManagerTest.js
@@ -1,24 +1,24 @@
 require("chai");
-var assert = require('assert');
-const auth = require('solid-auth-client');
+var assert = require("assert");
+const auth = require("solid-auth-client");
 
 const DataSync=require("../lib/datasync");
 
-const Core = require('../lib/core');
+const Core = require("../lib/core");
 const c = new Core(auth.fetch);
 
-const MessageManager = require('../lib/messageManager');
+const MessageManager = require(""../lib/messageManager");
 const chat = new MessageManager(c);
 
-describe('MessageManager Test', function () {
+describe("MessageManager Test", function () {
 
-    it('getNewMessage', function() {
+    it("getNewMessage", function() {
         var result=chat.getNewMessage("https://antoniete.solid.community/inbox/14532d20-594d-11e9-b3ac-bb6f656d2471.txt",  "/inbox/");
         assert(result, null);
   });
     
 
-    it('storeMessage', function() {
+    it("storeMessage", function() {
         var result=chat.storeMessage("https://antoniete.solid.community/public/chat_Josejuanjo","Antoniete","holaa","https://josejuanjo.solid.community/inbox",new DataSync(auth.fecth),true);    
         assert(result, null);
     });
